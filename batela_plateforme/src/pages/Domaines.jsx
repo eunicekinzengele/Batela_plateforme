3eccfd35e52e2178fae23611c488554ae6bf69ba
eunicekinzengele <eunicekinzengele0@gmail.com>
2023-04-02 15:48:44 +0200

    feat: add a footer

---
+++ batela_plateforme/src/pages/Domaines.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavBar from '../pages/NavBar';
+
+export default function Domaines() {
+  return (
+    <div className=' w-full'>
+      <NavBar/>
+    </div>
+  )
+}
